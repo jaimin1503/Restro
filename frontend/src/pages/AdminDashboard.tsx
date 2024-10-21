a2ef60bef849724d7a16a9b253a3449e4c51a7f2
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-10-21 21:10:34 +0530

    init

---
+++ frontend/src/pages/AdminDashboard.tsx
@@ -0,0 +1,11 @@
+import Button from "../components/UI/Button"
+
+const AdminDashboard = () => {
+	return (
+		<div>
+
+			<Button lable="Button" className={""} variant="primary" disbled={false} onClick={() => { }} />
+		</div>
+	)
+}
+export default AdminDashboard
